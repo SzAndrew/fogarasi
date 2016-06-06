c4c0b2fa0daba3fb308c0d0be87b61150d10b5e9
Szolnoki András <Szolnoki Andrs>
2016-06-06 17:07:30 +0200

    Contact

--- contact.php
+++ contact.php
@@ -1,11 +1,28 @@
 <div class="contact_left">
-	<h2>KAPCSOLAT, ELÉRHETŐSÉGEINK</h2>
 	<div class="contact_title">
+		<h2>KAPCSOLAT, ELÉRHETŐSÉGEINK</h2>
 		<p>FOGARASI AUTÓ</p>
-		<p>Cím: 1131 Budapest, Üteg utca 17/a.</p>
-		<p>Telefonszám: (06) 20 397 2902</p>
+		<p>Cím: <span>1131 Budapest, Üteg utca 17/a.</span></p>
+		<p>Telefonszám: <span>(06) 20 397 2902</span></p>
 	</div>
-	<div class="contact_fo"></div>
-	<div class="contact_fr"></div>
+	<div class="contact_fo">
+		<img src="images/ggg.bmp">
+		<p>FOGARASI OLIVÉR<br>
+		<span>gépkocsi eladás-bérbeadás,<br>
+		műszaki vizsgáztatás, forgalomba helyezés</span>
+		</p>
+		<p>Mobil: <span>(06) 20 397 2902</span></p>
+		<p>E-mail: <span>fogarasioliver@t-online.hu</span></p>
+	</div>
+	<div class="contact_fr">
+		<img src="images/fff.bmp">
+		<p>FOGARASI RÓZSA<br>
+		<span>biztosítás és hitelközvetítés</span>
+		</p>
+		<p>Mobil: <span>(06) 30 622 0016</span></p>
+		<p>E-mail: <span>fogarasioliver@t-online.hu</span></p>
+	</div>
 </div>
-<div class="contact_right"></div>
+<div class="contact_right">
+	<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2693.8446125015835!2d19.07597001170716!3d47.53188813294839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741db9384c5eba9%3A0x18de4b43741eadf1!2sBudapest%2C+%C3%9Cteg+u.+17a%2C+1139!5e0!3m2!1shu!2shu!4v1465224674559" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <div class="menu">
 	<div class="menubox"></div>
 	<nav>
-		<a class="link" href="/subpage:home.php"><?php print $XML->header->link1; ?></a>
-		<a class="link" href="/subpage:welcome.php"><?php print $XML->header->link2; ?></a>
-		<a class="link" href="/subpage:services.php"><?php print $XML->header->link3; ?></a>
-		<a class="link" href="/subpage:rental.php"><?php print $XML->header->link4; ?></a>
-		<a class="link" href="/subpage:usedcars.php"><?php print $XML->header->link5; ?></a>
-		<a class="link" href="/subpage:contact.php"><?php print $XML->header->link6; ?></a>
+		<a class="link" href="/subpage:home"><?php print $XML->header->link1; ?></a>
+		<a class="link" href="/subpage:welcome"><?php print $XML->header->link2; ?></a>
+		<a class="link" href="/subpage:services"><?php print $XML->header->link3; ?></a>
+		<a class="link" href="/subpage:rental"><?php print $XML->header->link4; ?></a>
+		<a class="link" href="/subpage:usedcars"><?php print $XML->header->link5; ?></a>
+		<a class="link" href="/subpage:contact"><?php print $XML->header->link6; ?></a>
 	</nav>
 </div>
 <div class="logo">

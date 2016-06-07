2f98724bf5a924ee84f085c2dc583dad6c108d2e
Szolnoki András <Szolnoki Andrs>
2016-06-07 16:34:24 +0200

    Rental

--- rental.php
+++ rental.php
@@ -1,1 +1,107 @@
-I am Rental
+<div class="rental">
+	<h2>BÉRAUTÓ SZOLGÁLTATÁSUNK, BÉRBEADÁS</h2>
+	<p>Cégünk tevékenységeihez tartozik az autó bérbeadás. Az alábbi gépkocsik közül választhat:</p>
+</div>
+<div class="new_rental">
+	<div class="rental_left">
+		<div class="rental_top">
+			<div class="car_title"><h2>VOLKSWAGEN PASSAT</h2></div>
+			<div class="car_line">
+				<div class="line_central"></div>
+			</div>
+		</div>
+		<div class="description">
+			<p><span>ÉVJÁRAT:</span> 1998, <span>MOTOR TÉRFOGAT:</span> 1.595 cm<sup>3</sup>. <span>SAJÁT TÖMEG:</span> 1560 kg. <span>ÖSSZTÖMEG:</span> 2.110 kg. <span>CSOMAGTARTÓ:</span> 495 liter. <span>KLÍMA:</span> digitális két zónás, <span>TELJESÍTMÉNY:</span> 75 KW/102 LE, <span>EXTRA FELSZERELTSÉG:</span> elektromos ablak elől, hátul, ülésmagasság állítás, középső kartámasz, állítható kormánykerék, utas és vezető oldali légzsák, tempomat, centrálzár, riasztó, fedélzeti computer. <span>KÜLTÉR:</span> xenon fényszoró.</p>
+		</div>
+		<div class="price_day">
+			<div class="days">
+				<p>1 - 9 napra</p>
+				<p>10 - 29 napra</p>
+				<p>több mint 30 napra</p>
+			</div>
+			<div class="prices">
+				<p>8 000,-Ft/nap</p>
+				<p>7 000,-Ft/nap</p>
+				<p>6 000,-Ft/nap</p>
+			</div>
+		</div>
+		<div class="rental_images">
+			<img src="images/kep1.jpg" class="small_images">
+			<img src="images/kep2.jpg" class="small_images">
+			<img src="images/kep3.jpg" class="small_images">
+			<img src="images/kep4.jpg" class="small_images">
+		</div>
+	</div>
+	<div class="rental_right">
+		<img src="images/kep1.jpg">
+	</div>
+</div>
+<div class="new_rental">
+	<div class="rental_left">
+		<div class="rental_top">
+			<div class="car_title"><h2>VOLKSWAGEN PASSAT</h2></div>
+			<div class="car_line">
+				<div class="line_central"></div>
+			</div>
+		</div>
+		<div class="description">
+			<p><span>ÉVJÁRAT:</span> 1998, <span>MOTOR TÉRFOGAT:</span> 1.595 cm<sup>3</sup>. <span>SAJÁT TÖMEG:</span> 1560 kg. <span>ÖSSZTÖMEG:</span> 2.110 kg. <span>CSOMAGTARTÓ:</span> 495 liter. <span>KLÍMA:</span> digitális két zónás, <span>TELJESÍTMÉNY:</span> 75 KW/102 LE, <span>EXTRA FELSZERELTSÉG:</span> elektromos ablak elől, hátul, ülésmagasság állítás, középső kartámasz, állítható kormánykerék, utas és vezető oldali légzsák, tempomat, centrálzár, riasztó, fedélzeti computer. <span>KÜLTÉR:</span> xenon fényszoró.</p>
+		</div>
+		<div class="price_day">
+			<div class="days">
+				<p>1 - 9 napra</p>
+				<p>10 - 29 napra</p>
+				<p>több mint 30 napra</p>
+			</div>
+			<div class="prices">
+				<p>8 000,-Ft/nap</p>
+				<p>7 000,-Ft/nap</p>
+				<p>6 000,-Ft/nap</p>
+			</div>
+		</div>
+		<div class="rental_images">
+			<img src="images/kep1.jpg" class="small_images">
+			<img src="images/kep2.jpg" class="small_images">
+			<img src="images/kep3.jpg" class="small_images">
+			<img src="images/kep4.jpg" class="small_images">
+		</div>
+	</div>
+	<div class="rental_right">
+		<img src="images/kep1.jpg">
+	</div>
+</div>
+<div class="new_rental">
+	<div class="rental_left">
+		<div class="rental_top">
+			<div class="car_title"><h2>VOLKSWAGEN PASSAT</h2></div>
+			<div class="car_line">
+				<div class="line_central"></div>
+			</div>
+		</div>
+		<div class="description">
+			<p><span>ÉVJÁRAT:</span> 1998, <span>MOTOR TÉRFOGAT:</span> 1.595 cm<sup>3</sup>. <span>SAJÁT TÖMEG:</span> 1560 kg. <span>ÖSSZTÖMEG:</span> 2.110 kg. <span>CSOMAGTARTÓ:</span> 495 liter. <span>KLÍMA:</span> digitális két zónás, <span>TELJESÍTMÉNY:</span> 75 KW/102 LE, <span>EXTRA FELSZERELTSÉG:</span> elektromos ablak elől, hátul, ülésmagasság állítás, középső kartámasz, állítható kormánykerék, utas és vezető oldali légzsák, tempomat, centrálzár, riasztó, fedélzeti computer. <span>KÜLTÉR:</span> xenon fényszoró.</p>
+		</div>
+		<div class="price_day">
+			<div class="days">
+				<p>1 - 9 napra</p>
+				<p>10 - 29 napra</p>
+				<p>több mint 30 napra</p>
+			</div>
+			<div class="prices">
+				<p>8 000,-Ft/nap</p>
+				<p>7 000,-Ft/nap</p>
+				<p>6 000,-Ft/nap</p>
+			</div>
+		</div>
+		<div class="rental_images">
+			<img src="images/kep1.jpg" class="small_images">
+			<img src="images/kep2.jpg" class="small_images">
+			<img src="images/kep3.jpg" class="small_images">
+			<img src="images/kep4.jpg" class="small_images">
+		</div>
+	</div>
+	<div class="rental_right">
+		<img src="images/kep1.jpg">
+	</div>
+</div>
+<div class="clean"></div>
